6448e4c789935c1121c217ad69e631214ea12aec
Plasticon Tech <Plasticon Tech@DESKTOP-NUGAIUG>
2021-06-25 09:39:38 +0530

    Hello

--- src/main/java/com/plasticon/erp/controller/ScholasticGradeMarksSetupController.java
+++ src/main/java/com/plasticon/erp/controller/ScholasticGradeMarksSetupController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.plasticon.erp.model.ScholasticGradeMarksSetupDetails;
 import com.plasticon.erp.service.ScholasticGradeMarksSetupService;
 
-
-
+@RestController
+@RequestMapping("/SGMSS")
 public class ScholasticGradeMarksSetupController {
 	
 	@Autowired
