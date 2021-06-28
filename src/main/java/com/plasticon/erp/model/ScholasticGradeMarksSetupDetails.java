3a41548a517df13eb9f64e9d88e9c1ea41072cbc
Plasticon Tech <Plasticon Tech@DESKTOP-NUGAIUG>
2021-06-28 12:09:25 +0530

    changes module

--- src/main/java/com/plasticon/erp/model/ScholasticGradeMarksSetupDetails.java
+++ src/main/java/com/plasticon/erp/model/ScholasticGradeMarksSetupDetails.java
@@ -1,8 +1,17 @@
 package com.plasticon.erp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+
 
 import lombok.Data;
 
@@ -19,5 +28,8 @@ public class ScholasticGradeMarksSetupDetails {
 	private int minimum;
 	private String result;
 	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "gid_fk", referencedColumnName = "sgmsId")
+	private List<ScholasticGradeCreateDetails> details = new ArrayList<>();
 
 }
