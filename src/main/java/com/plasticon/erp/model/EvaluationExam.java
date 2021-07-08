6c4f0d4e8e1dd30424cc10609abd7da25b8bcc4d
plasticpontechnologies <79623637+plasticpontechnologies@users.noreply.github.com>
2021-07-08 14:41:38 +0530

    Update EvaluationExam.java

--- src/main/java/com/plasticon/erp/model/EvaluationExam.java
+++ src/main/java/com/plasticon/erp/model/EvaluationExam.java
@@ -10,12 +10,16 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "exam_student_list")
-public class EvaluationExam {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int slno;
-	private String Exam;
+@Table(name="evaluate_result")
+public class EvaluateExamResult {
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	    private int slno;
+	    private String onlineExam;
+	    private String classes;
+	    private String Student;
+
 
 
 }
+
