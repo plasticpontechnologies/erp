75036495eae02ec57fb80c440162e7e958997e3e
saishanker41@gmail.com <saishanker41@gmail.com>
2021-07-09 08:57:19 +0530

    deleted unwanted methods

--- src/main/java/com/plasticon/erp/model/classes.java
+++ src/main/java/com/plasticon/erp/model/classes.java
@@ -4,22 +4,15 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+
 
 @Entity
 @Data
