1f6b65851db25084e6987a86620be867fe86a1ee
plasticpontechnologies <79623637+plasticpontechnologies@users.noreply.github.com>
2021-07-28 15:22:13 +0530

    Update FileController.java

--- src/main/java/com/plasticon/erp/controller/FileController.java
+++ src/main/java/com/plasticon/erp/controller/FileController.java
@@ -1,17 +1,23 @@
 package com.plasticon.erp.controller;
 
+import java.io.IOException;
+import java.sql.Blob;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,36 +35,41 @@ import com.plasticon.erp.service.FileStorageService;
 @RestController
 @RequestMapping("/upload")
 public class FileController {
-	
+
 	@Autowired
 	private FileStorageService fileStorageService;
 
-	@PostMapping(value="/uploadFile")
-	public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+	@PostMapping(value = "/uploadFile")
+	public String uploadFile(@RequestParam("file") MultipartFile file) {
 		FileModel fileModel = fileStorageService.storeFile(file);
-
-		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFile/")
-				.path(fileModel.getId()).toUriString();
-		UploadFileResponse uploadFileResponse = new UploadFileResponse();
-		uploadFileResponse.setFileName(fileModel.getFileName());	
-		uploadFileResponse.setFileType(file.getContentType());
-		uploadFileResponse.setSize(file.getSize());
-		uploadFileResponse.setFileDownloadUri(fileDownloadUri);
-		return uploadFileResponse;
+		return "Success";
 	}
 
-	@PostMapping(value = "/uploadMultipleFiles", consumes = "application/json")
-	public List<UploadFileResponse> uploadMulitpleFiles(@RequestParam("file") MultipartFile[] files) {
+	@PostMapping(value = "/uploadMultipleFiles")
+	public List<String> uploadMulitpleFiles(@RequestParam("file") MultipartFile[] files) {
 		return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
 	}
 
-	@GetMapping(value= "/downloadFile/{fileId}")
-	public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
+	
 
-		FileModel fileModel = fileStorageService.getFile(fileId);
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(fileModel.getFileType()))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileModel.getFileName() + "\"")
-				.body(new ByteArrayResource(fileModel.getData())); 
+
+
+	@DeleteMapping(value = "/delete/{fileId}")
+	public void delete(@PathVariable int fileId) {
+		fileStorageService.deleteFile(fileId);
+	}
+
+	@GetMapping(value = "/get/{fileId}")
+	public void get(@PathVariable int fileId, HttpServletResponse response) throws IOException {
+		response.setContentType("image/jpeg");
+		Optional<FileModel> fil = fileStorageService.getFile1(fileId);
+		if (fil.isPresent()) {
+
+			byte[] image = fil.get().getData();
+
+			StreamUtils.copy(image, response.getOutputStream());
+		}
+
 	}
 
 }
