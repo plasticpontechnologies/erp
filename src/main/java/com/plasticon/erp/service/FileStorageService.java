9e2c1ef1284dea56b582739b3f5b44c93dc00b72
plasticpontechnologies <79623637+plasticpontechnologies@users.noreply.github.com>
2021-07-28 15:08:32 +0530

    Update FileStorageService.java

--- src/main/java/com/plasticon/erp/service/FileStorageService.java
+++ src/main/java/com/plasticon/erp/service/FileStorageService.java
@@ -1,14 +1,17 @@
 package com.plasticon.erp.service;
 
+
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.plasticon.erp.model.FileModel;
-
 @Service
 public interface FileStorageService {
 	
 	public FileModel storeFile(MultipartFile file);
-	public FileModel getFile(String fileId);
-
+	public FileModel getFile(int fileId);
+	public void deleteFile(int fileId);
+	public Optional<FileModel> getFile1(int fileId); 
+	public List<FileModel> getFiles();
 }
